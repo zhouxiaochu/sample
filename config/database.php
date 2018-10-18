ddf3055c6214b0bad8c760771bb48b7f656d1591
李亚洲 <2263145686@qq.com>
2018-10-18 14:58:32 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,19 +1,10 @@
 <?php
 
+$db_config = get_db_config();
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
