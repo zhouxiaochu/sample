31805c5a0dc1365abcc621e971a05f85e6b471d2
李亚洲 <2263145686@qq.com>
2018-10-24 14:46:05 +0000

    Add password resets & email configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword;
 
 class User extends Authenticatable
 {
@@ -14,6 +15,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+
 
     protected $table = 'users';
     
@@ -40,11 +43,17 @@ class User extends Authenticatable
         });
     }
 
-    
+
 
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    
+     public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 }
